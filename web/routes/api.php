fbb583cd40a9bff1fe734d5494527e380e4a5a7d
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-03-10 11:17:51 +0000

    feat: add route for retrieving reservations in the API

--- web/routes/api.php
+++ web/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\StationController;
 use App\Models\Station;
 use GuzzleHttp\Middleware;
@@ -13,6 +14,8 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
 Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+// stations
 Route::get('/stations',[StationController::class,'index']);
 
     Route::middleware('admin')->group(function(){
@@ -20,4 +23,7 @@ Route::get('/stations',[StationController::class,'index']);
         Route::put('/stations/{id}',[StationController::class,'update']);
         Route::delete('/stations/{id}',[StationController::class,'destroy']);
     });
+
+// reservation
+Route::get('/reservations',[ReservationController::class,'index']);
 });
